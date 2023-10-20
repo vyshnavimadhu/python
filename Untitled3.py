b96bd9e81e97b2574466e0823172d50d566f666d
vyshnavimadhu <145978493+vyshnavimadhu@users.noreply.github.com>
2023-10-20 12:07:13 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+list1=[]
+for i in range(0,5):
+    n=int(input("enter the integer:"))
+    list1.append(n)
+print(list1)
+for i in list1:
+    if(i%2==0):
+        list1.remove(i)
+print("after removing even number:",list1)        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
