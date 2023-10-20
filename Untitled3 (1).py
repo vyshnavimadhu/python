13df67b215715c18db27783621e17ea8bdb28183
vyshnavimadhu <145978493+vyshnavimadhu@users.noreply.github.com>
2023-10-20 12:07:39 +0530

    Add files via upload

---
+++ Untitled3 (1).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+current=int(input("enter the current year"))
+final=int(input("enter the final year"))
+print("the leap years are:")
+for i in range(current,final):
+    if(i%400==0 or (i%100!=0 and i%4==0)):
+        print(i)        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
