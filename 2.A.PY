e80992dd0a0ebd3498e6ca320ef93a1a1029b164
vyshnavimadhu <145978493+vyshnavimadhu@users.noreply.github.com>
2023-11-29 20:38:29 +0530

    Add files via upload

---
+++ 2.A.PY
@@ -0,0 +1,4 @@
+r=int(input("Enter the radius of the circle:"))
+pi=3.14
+area=pi*r*r
+print("Area of the circle is=" '%.2f'%area)
---
+++ 2.C.PY
@@ -0,0 +1,5 @@
+n=int(input("Enter an number:"))
+n=str(n)
+nn=n+n
+nnn=n+n+n
+sum=n+nn+nnn
